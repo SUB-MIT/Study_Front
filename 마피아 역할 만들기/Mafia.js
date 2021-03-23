80593455fde16c2060f1923fc33c0dac4d1913f0
Kimjimin0807 <jm004471@naver.com>
2021-03-23 23:24:29 +0900

    [CHANGE] <Mafia.js> 코드수정

--- 마피아 역할 만들기/Mafia.js
+++ 마피아 역할 만들기/Mafia.js
@@ -1,9 +1,11 @@
 document.querySelector(".logoImg").onclick = function() {
     document.querySelector(".black").style.visibility="visible";
     document.querySelector(".card").style.visibility="visible";
+    document.querySelector(".mainBox").style.visibility="hidden";
 }
 
 document.querySelector("#hideBtn").onclick = function() {
     document.querySelector(".black").style.visibility="hidden";
     document.querySelector(".card").style.visibility="hidden";
+    document.querySelector(".mainBox").style.visibility="visible";
 }
